82799fe88de102b9d3202086e3bed761ea628df4
fadhlirabbi <Fadhlirabbi46@gmail.com>
2025-04-07 18:33:05 +0700

    menambahkan class KuliahMahasiswa103022300055

--- Program.cs
+++ Program.cs
@@ -4,5 +4,8 @@
     {
         DataMahasiswa103022300055 data = new DataMahasiswa103022300055();
         data.ReadJSON();
+
+        var km = new KuliahMahasiswa103022300055();
+        km.ReadJSON();
     }
 }
