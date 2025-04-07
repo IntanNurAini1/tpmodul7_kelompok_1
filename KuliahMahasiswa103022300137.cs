2e2592affccd27816e6c89e6ab7eb67f8ff4fe4b
gmii <gumilarharisubagja28@gmail.com>
2025-04-07 19:00:42 +0700

    Menambahkan class KuliahMahasiswa103022300137

--- KuliahMahasiswa103022300137.cs
+++ KuliahMahasiswa103022300137.cs
@@ -1,12 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
 
-namespace tpmodul7_kelompok_1
+public class KuliahMahasiswa103022300137
 {
-    class KuliahMahasiswa103022300137
+    public class MataKuliah
     {
+        public string name { get; set; }
+        public string code { get; set; }
+    }
+
+    public List<MataKuliah> courses { get; set; }
+
+    public void ReadJSON()
+    {
+        string path = "tp7_2_103022300137.json";
+
+        try
+        {
+            string jsonData = File.ReadAllText(path);
+            var data = JsonSerializer.Deserialize<KuliahMahasiswa103022300137>(jsonData);
+
+            Console.WriteLine("Daftar mata kuliah yang diambil:");
+            for (int i = 0; i < data.courses.Count; i++)
+            {
+                Console.WriteLine($"MK {i + 1} {data.courses[i].code} - {data.courses[i].name}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
     }
 }
