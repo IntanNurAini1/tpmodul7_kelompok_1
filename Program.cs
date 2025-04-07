e3dbb5cbc7a83e51006f4bdf4b154d356351adeb
gmii <gumilarharisubagja28@gmail.com>
2025-04-07 18:59:55 +0700

    Menambahkan class DataMahasiswa103022300137

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        var dataGumi = new DataMahasiswa103022300137();
+        Console.WriteLine("Memulai proses membaca data mahasiswa...");
+        dataGumi.ReadJSON();
+        Console.WriteLine("Proses selesai.");
+
+        Console.WriteLine("");
+
+        Console.WriteLine("Memulai proses membaca data mata kuliah...");
+        var kuliahGumi = new KuliahMahasiswa103022300137();
+        kuliahGumi.ReadJSON();
+        Console.WriteLine("Proses selesai.");
+    }
+}
