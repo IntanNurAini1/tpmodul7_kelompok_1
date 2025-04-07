9ec12f6160ca382fa6c4bded533ff89b12fae4d9
IntanNurAini <intan.nuraini2005@gmail.com>
2025-04-07 19:02:49 +0700

    menambahkan class KuliahMahasiswa103022330067

--- Program.cs
+++ Program.cs
@@ -4,10 +4,10 @@
     {
         var mahintan = new DataMahasiswa103022330067();
         mahintan.ReadJSON();
-        //Console.WriteLine();
-        //Console.WriteLine("====================================");
-    //    var kuliahintan = new KuliahMahasiswa103022330067();
-    //    kuliahintan.ReadJSON();
-    //    Console.WriteLine("====================================");
+        Console.WriteLine();
+        Console.WriteLine("====================================");
+        var kuliahintan = new KuliahMahasiswa103022330067();
+        kuliahintan.ReadJSON();
+        Console.WriteLine("====================================");
     }
 }
