85b143356e61fe1f1fe245e68bedf8c44bd0a136
agungperamerta <gedeagung068@gmail.com>
2025-04-07 17:45:22 +0700

    menambhakan class DataMahasiswa103022300087

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           DataMahasiswa103022300087 DataAgung = new DataMahasiswa103022300087();
+           DataAgung.ReadJSON();
+        }
+    }
+
