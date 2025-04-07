570e2a16805ee35f12a4a911b146ad7001cca236
IntanNurAini <intan.nuraini2005@gmail.com>
2025-04-07 19:02:07 +0700

    menambahkan class DataMahasiswa103022330067

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿class Program
+{
+    static void Main()
+    {
+        var mahintan = new DataMahasiswa103022330067();
+        mahintan.ReadJSON();
+        //Console.WriteLine();
+        //Console.WriteLine("====================================");
+    //    var kuliahintan = new KuliahMahasiswa103022330067();
+    //    kuliahintan.ReadJSON();
+    //    Console.WriteLine("====================================");
+    }
+}
