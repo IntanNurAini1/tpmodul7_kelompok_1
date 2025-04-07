0ce1c66e4a28ea2da07135f0b44a89a84e5a4beb
Steven Gerald <stevengerald9924@gmail.com>
2025-04-07 18:55:08 +0700

    Menambahkan Kelas DataMahasiswa103022300155

--- Program.cs
+++ Program.cs
@@ -5,6 +5,11 @@
         var DataSteven = new DataMahasiswa103022300155();
         Console.WriteLine("Memulai proses membaca data mahasiswa...");
         DataSteven.ReadJSON();
+        Console.WriteLine("Proses selesai.\n");
+
+        Console.WriteLine("Memulai proses membaca data mata kuliah...");
+        var KuliahSteven = new KuliahMahasiswa103022300155();
+        KuliahSteven.ReadJSON();
         Console.WriteLine("Proses selesai.");
     }
 }
