9de80f65720fee979d2812816e614e1fe302f0db
baalaal <147219396+baalaal@users.noreply.github.com>
2025-04-07 19:12:22 +0700

    Menambahkan class DataMahasiswa103022300146

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var DataIqbal = new DataMahasiswa103022300146();
+
+        Console.WriteLine("Memulai proses membaca data mahasiswa...");
+        DataIqbal.ReadJSON();
+        Console.WriteLine("Proses selesai.\n\n");
+    }
+}
